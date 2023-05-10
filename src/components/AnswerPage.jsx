639566bdc0a9f81942b081484efe3e6c564a0884
ploy-thanaporn <tnpsp1501@gmail.com>
2023-05-10 10:58:29 +0700

    style: answer question

--- src/components/AnswerPage.jsx
+++ src/components/AnswerPage.jsx
@@ -5,19 +5,24 @@ const AnswerPage = () => {
   const { questionAnswers } = useContext(QuestionContext);
 
   return (
-    <div className="px-4 mt-5 text-slate-600">
-      <h1 className="text-red-500 font-bold text-lg">Answer</h1>
-      <div className="mt-2">
+    <div className="mt-5 text-slate-800  mx-10 border px-10 py-5 rounded-md">
+      <h1 className="font-bold text-lg">Answer</h1>
+      <div className="mt-3 ml-2">
         {questionAnswers.map((question, index) => (
           <div className="flex">
             <span className="mr-2">{`${index + 1}.`}</span>
-            {question.type === "checkbox" ? (
-              question.choicesSelected.map((choice) => (
-                <div className="pr-1">{choice.title}</div>
-              ))
-            ) : (
-              <div>{question.answer}</div>
-            )}
+            <div className="flex flex-col mb-2.5">
+              {question.title}
+              {question.type === "checkbox" ? (
+                <div className="text-slate-500">
+                  {question.choicesSelected
+                    .map((choice) => choice.title)
+                    .join(", ")}
+                </div>
+              ) : (
+                <div className="text-slate-500">{question.answer}</div>
+              )}
+            </div>
           </div>
         ))}
       </div>
